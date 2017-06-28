69cd4dce27beb69eddc74a9844efa1db0c6640fc
afox <0x79d.m@gmail.com>
2017-06-28 23:09:23 +0800

    June
    Analysis of iOS.GuiInject Adware Library

---
+++ XIN/DailyUploadDownloadLib/DailyUploadDownloadLib/DailyUploadDownloadLib.h
@@ -0,0 +1,24 @@
+//
+//  DailyUploadDownloadLib.h
+//  DailyUploadDownloadLib
+//
+//  Created by Sinbad on 2017/6/28.
+//  Copyright © 2017年 kanxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DailyUploadDownloadLib.
+FOUNDATION_EXPORT double DailyUploadDownloadLibVersionNumber;
+
+//! Project version string for DailyUploadDownloadLib.
+FOUNDATION_EXPORT const unsigned char DailyUploadDownloadLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DailyUploadDownloadLib/PublicHeader.h>
+
+
+@interface DailyUploadDownloadLib : NSObject
+
+- (void)run;
+
+@end
